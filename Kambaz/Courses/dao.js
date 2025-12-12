365dd8d12b36da2e2dadc5d21bfcbba24d0cf3bf
Dimcirui <dimcirui@qq.com>
2025-12-11 20:11:08 -0500

    refactor: format deleteCourse function for improved readability

--- Kambaz/Courses/dao.js
+++ Kambaz/Courses/dao.js
@@ -15,9 +15,9 @@ function createCourse(course) {
   const newCourse = { ...course, _id: uuidv4() };
   return model.create(newCourse);
 }
-  function deleteCourse(courseId) {
-    return model.deleteOne({ _id: courseId });
-  }
+function deleteCourse(courseId) {
+  return model.deleteOne({ _id: courseId });
+}
 function updateCourse(courseId, courseUpdates) {
   return model.updateOne({ _id: courseId }, { $set: courseUpdates });
 }
